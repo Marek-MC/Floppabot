f799fcc9b27d35d6741824a5bfb2c7dcd0554d3c
Marek-MC <76727652+Marek-MC@users.noreply.github.com>
2022-06-06 14:59:07 -0500

    First commit

---
+++ src/origin.py
@@ -0,0 +1,21 @@
+import discord
+import json
+
+# Literally just imports the Token from Json.
+with open('TOKEN.json', 'r') as f:
+    data=json.load(f);token=data['TOKEN'];f.close() #
+    
+# Create the main client function
+class MyClient(discord.Client):
+    async def on_ready(self): 
+        print('Logged on as {0}!'.format(self.user))
+    
+    async def on_message(self, message): # When client recieves a message
+        print('{0.author}: {0.content}'.format(message))
+
+
+
+
+# Startup
+MyClient().run(token)
+
---
+++ start.py
@@ -0,0 +1,8 @@
+import subprocess
+
+# Main runs the process. 
+def main():
+    subprocess.call("python3 ./src/origin.py",shell=True)
+
+if __name__ == "__main__":
+    main()
